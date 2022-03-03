ec24ffdefb9cf64afaabdab85690de67c67b489b
dcnis <bgk.dennis@yahoo.de>
2022-03-03 15:34:17 +0800

    added time to dubbo-provider-1

--- dubbo-provider-1/src/main/java/de/dcnis/dubbo/provider/DemoServiceImpl.java
+++ dubbo-provider-1/src/main/java/de/dcnis/dubbo/provider/DemoServiceImpl.java
@@ -3,11 +3,18 @@ package de.dcnis.dubbo.provider;
 import de.dcnis.api.DemoService;
 import org.apache.dubbo.config.annotation.DubboService;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @DubboService(version = "1.0.0")
 public class DemoServiceImpl implements DemoService {
 
     @Override
     public String sayHello(String name) {
+        LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm:ss");
+        System.out.println("Provider answered " + time.format(format));
+
         return "Hello " + name + " from Docker";
     }
 
