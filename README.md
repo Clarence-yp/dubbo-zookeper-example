80b710bbb05bcf89da18b77479a4e20e600968cc
dcnis <bgk.dennis@yahoo.de>
2022-03-03 17:12:52 +0800

    added ApplicationContext to method in order to distinguish instances

--- README.md
+++ README.md
@@ -45,19 +45,25 @@ Here we spin up an instance of ```dubbo-consumer-1```
 docker run --name dubbo-consumer-1-0 -d -p 8090:8080 -p 28880:28880 --link zkserver:zkserver dubbo-consumer-1-image
 ```
 
+## Spin up zookeeper as Service Registry Center
+```
+docker run --name zookeeper --restart always -d zookeeper:3.4.9
+```
+
 ## Zookeeper GUI
 
 ```ZK_DEFAULT_NODE``` is the IP-address of the **zookeeper docker container**.
+How to get the IP-address of a docker container, will be explained below.
 
 ```
 docker run -d \
   -p 8086:8080 \
   -e ZK_DEFAULT_NODE=172.17.0.2:2181/ \
-  --name zk-web \
+  --name zookeeper-gui \
   -t tobilg/zookeeper-webui
 ```
 
-## How to get Docker IP-address
+## How to get IP-address of docker container
 
 ```
 docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' container_name_or_id
--- dubbo-provider-1/src/main/java/de/dcnis/dubbo/provider/DemoServiceImpl.java
+++ dubbo-provider-1/src/main/java/de/dcnis/dubbo/provider/DemoServiceImpl.java
@@ -2,6 +2,8 @@ package de.dcnis.dubbo.provider;
 
 import de.dcnis.api.DemoService;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -9,13 +11,16 @@ import java.time.format.DateTimeFormatter;
 @DubboService(version = "1.0.0")
 public class DemoServiceImpl implements DemoService {
 
+    @Autowired
+    private ApplicationContext appContext;
+
     @Override
     public String sayHello(String name) {
         LocalDateTime time = LocalDateTime.now();
         DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm:ss");
         System.out.println("Provider answered " + time.format(format));
 
-        return "Hello " + name + " from Docker";
+        return "Hello " + name + " from Docker " + appContext.getDisplayName();
     }
 
 }
